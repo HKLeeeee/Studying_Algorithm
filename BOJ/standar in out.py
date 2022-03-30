a4f77def4dc50a326762a2c1f963536fa506fbee
HKLeeeee <dlapdlf1739@gmail.com>
2022-03-30 19:41:01 +0900

    2020-03-30

--- BOJ/standar in out.py
+++ BOJ/standar in out.py
@@ -1,33 +1,47 @@
 import sys
 
+
 def vps(ps):
     stack = []
 
-    for v in ps :
+    for v in ps:
         if v == "(" or v == "[":
             stack.append(v)
         elif v == ")":
-            if len(stack) == 0 :
+            if len(stack) == 0:
                 return False
-            else :
-                if stack[-1] == "(" :
+            else:
+                if stack[-1] == "(":
                     stack.pop()
-                else :
+                else:
                     return False
         elif v == "]":
-            if len(stack) == 0 :
+            if len(stack) == 0:
                 return False
-            else :
-                if stack[-1] == "[" :
+            else:
+                if stack[-1] == "[":
                     stack.pop()
-                else :
+                else:
                     return False
 
-    if len(stack) != 0 :
+    if len(stack) != 0:
         print(3)
         return False
 
     return True
+
+
+def fibo(n):
+    global call_0
+    global call_1
+    if n == 0:
+        call_0 += 1
+        return n
+    elif n == 1:
+        call_1 += 1
+        return n
+    else:
+        return fibo(n - 1) + fibo(n - 2)
 
 
 if __name__ == '__main__':
@@ -40,9 +54,19 @@ if __name__ == '__main__':
     #
     # for x in lst:
     #     sys.stdout.write(str(num.index(x)) + " ")
+    #
+    # while True:
+    #     t = sys.stdin.readline()
+    #     if t == ".\n" or t == ".":
+    #         break
+    #     print("yes" if vps(t) else "no")
 
-    while True:
-        t = sys.stdin.readline()
-        if t == ".\n" or t == ".":
-            break
-        print("yes" if vps(t) else "no")
+    input = sys.stdin.readline
+
+    x = int(input())
+
+    for i in range(x):
+        call_0 = 0
+        call_1 = 0
+        result = fibo(int(input()))
+        sys.stdout.write(str(call_0)+" "+str(call_1))
